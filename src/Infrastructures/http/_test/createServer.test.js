f1f749f9206c8c41a37680739cae1dea7936bff2
Geraldo Sepdwijaya <geraldo.sepdwijaya@maybank.co.id>
2025-04-09 00:29:45 +0700

    add hello world feature

--- src/Infrastructures/http/_test/createServer.test.js
+++ src/Infrastructures/http/_test/createServer.test.js
@@ -42,7 +42,7 @@ describe('HTTP server', () => {
       // Assert
       const responseJson = JSON.parse(response.payload);
       expect(response.statusCode).toEqual(200);
-      expect(responseJson.value).toEqual('Hello world hihi');
+      expect(responseJson.value).toEqual('Hello world!');
     });
   });
 
